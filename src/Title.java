45f95adb270c7b7c1ba8d0efb2b99a8c0b7e39ad
Paweł Korniak <pawello1020@gmail.com>
2020-11-08 14:08:20 +0100

    working GuessTheTitle game

--- src/Title.java
+++ src/Title.java
@@ -1,32 +1,44 @@
 public class Title {
-    char [] toShow= {' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
-    String name;
-    int added;
+    char [] toShow= {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};//empty chars for printing correct letters guessed
+    String name;    // title
+    int added;  // correct letters counter for toShow array
 
-    void addChar(char c){
-        this.toShow[added]=c;
-        added++;
+    boolean addChar(char c){ /// get char to chceck if it's in title, if it is , returns true
+
+        boolean isInTitle=false;
+        for(int i=0;i<=this.name.length()-1;i++){
+            if(this.name.charAt(i)==c){   // comparing every char of string
+                isInTitle=true;
+            }
+        }
+        if(isInTitle){
+            this.toShow[added]=c; // if title contains argument, is added to array
+            added++;
+        }
+        return isInTitle;
     }
-    void printTitle(){
+    void printTitle(){ // printing title with '_' , ' ' and letters added by addChar function
+
         for(int i=0;i<=(name.length()-1);i++){
             boolean isAdded=false;
             for(int j=0;j<added;j++){
 
-                if(name.charAt(i) == toShow[j]){
+                if((name.charAt(i) == toShow[j])&&!isAdded){ // additional isAdded in case of multiple similar letters added
                     System.out.print(name.charAt(i));
                     isAdded=true;
                 }
 
             }
-            if(!(isAdded) && name.charAt(i)!=' '){
+            if(!(isAdded)&&!(name.charAt(i)==' ')){  // if nothing found, print '_'
                 System.out.print('_');
-            } else System.out.print(' ');
+            }
+            if(name.charAt(i)==' ') System.out.print(' '); // spaces are also importatnt to guess title
         }
         System.out.println();
     }
-    Title(String name){
+    Title(String name){ // constructor
         this.name=name;
         added = 0;
-        //toShow = {' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
+
     }
 }
--- src/guessTheTitle.java
+++ src/guessTheTitle.java
@@ -2,35 +2,39 @@ import java.io.File;
 import java.util.Scanner;
 
 public class guessTheTitle {
-    //function will add next char to the title to show , unadded chars wi be '_'
+
     public static void main(String [] args) throws Exception{
         File file = new File("text.txt");
         Scanner scanner = new Scanner(file);
         Scanner read = new Scanner(System.in);
         int linesCount=0;
-        while(scanner.hasNextLine()){
+        while(scanner.hasNextLine()){ //counting lines to get range of randomization
             scanner.nextLine();
             linesCount ++;
         }
 
         int titleNumber = (int) (Math.random() * linesCount);
         Scanner titleScanner = new Scanner(file);
-        for(int i=0;i<titleNumber;i++)titleScanner.nextLine();
+        for(int i=0;i<titleNumber;i++)titleScanner.nextLine(); // go to random line in file
 
-        Title titleToGuess = new Title(titleScanner.nextLine());
+        Title titleToGuess = new Title(titleScanner.nextLine()); // set random line to Title object
 
-
+        int mistakes=0;
         boolean guessed=false;
-        while(!(guessed)){
-            System.out.println(titleToGuess.name);
+        while(!(guessed) && mistakes<10){
+            //System.out.println(titleToGuess.name);   //  just for testing
             titleToGuess.printTitle();
             System.out.print("Guess letter or Title : ");
             String guess= read.nextLine();
             System.out.println();
-            if(guess.length()==1){
-                titleToGuess.addChar(guess.charAt(0));
-            }
-        }
+            if(guess.length()==1){ // if user enter one letter it is used in addChar to check if is in title
+                if(!(titleToGuess.addChar(guess.charAt(0))))mistakes++; //addChar run and returns false if letter is not in title, then it is counted as mistake
+            } else if(guess.equals(titleToGuess.name)){ // win condition
+                guessed = true;
 
+            }else mistakes++;
+        System.out.println("Mistakes - "+mistakes);
+        }
+        if(guessed)System.out.println("You are right"); else System.out.println("out of chances");
     }
 }
